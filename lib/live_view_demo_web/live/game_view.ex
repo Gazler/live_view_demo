9c77a6b2829c2482e61b5089717a191c70dcb4d8
Tomasz Kowal <tomasz.kowal@clubcollect.com>
2019-09-13 13:28:45 +0200

    Remove old version of live view

--- lib/live_view_demo_web/live/game_view.ex
+++ lib/live_view_demo_web/live/game_view.ex
@@ -1,6 +1,5 @@
 defmodule LiveViewDemoWeb.GameLive do
   use Phoenix.LiveView
-  import Phoenix.HTML.Form
   alias LiveViewDemo.Games
 
   defmodule PlayerInput do
@@ -15,78 +14,44 @@ defmodule LiveViewDemoWeb.GameLive do
         <%= @puzzle %>
       </div>
       <div style="text-align: center; font-size: 2em; width: 100%;">
-      <%= f = form_for @changeset, "#", [phx_change: "guess"] %>
-        <%= text_input f, :guessed_number %>
-      </form>
-      </div
+      <p>TODO PRINT GUESS</p>
+      </div>
     </div>
     """
   end
 
   def mount(_session, socket) do
-    {:ok, pid} = Games.new()
-    if connected?(socket), do: :timer.send_interval(1000, self(), :tick)
-
     socket =
       socket
-      |> assign(%{
-        changeset: player_input_changeset(%{}),
-        server_pid: pid,
-        remaining_time: 0,
-        puzzle: ""
-      })
+      |> maybe_start_game()
+      |> assign(%{remaining_time: 0, puzzle: ""})
 
     {:ok, socket}
   end
 
-  def handle_event("guess", %{"player_input" => %{"guessed_number" => ""}}, socket) do
-    {:noreply, socket}
-  end
+  defp maybe_start_game(socket) do
+    if connected?(socket) do
+      {:ok, game_handle} = Games.new()
+      {:ok, timer_reference} = :timer.send_interval(1000, self(), :tick)
 
-  def handle_event("guess", %{"player_input" => player_input}, socket) do
-    pid = socket.assigns.server_pid
-
-    changeset =
-      player_input
-      |> player_input_changeset()
-
-    {correct_or_incorrect, %{remaining_time: remaining_time, puzzle: puzzle}} =
-      Games.player_input(pid, changeset.changes.guessed_number)
-
-    changeset =
-      case correct_or_incorrect do
-        :correct ->
-          # Ecto.Changeset.put_change(changeset, :guessed_number, "")
-          player_input_changeset(%{})
-
-        :incorrect ->
-          changeset
-      end
-
-    {:noreply,
-     assign(
-       socket,
-       %{changeset: changeset, remaining_time: remaining_time, puzzle: puzzle}
-     )}
+      socket
+      |> assign(%{
+        timer_reference: timer_reference,
+        game_handle: game_handle
+      })
+    else
+      socket
+    end
   end
 
   def handle_info(:tick, socket) do
-    pid = socket.assigns.server_pid
-
-    case Games.tick(pid) do
+    case Games.tick(socket.assigns.game_handle) do
       {:continue, game} ->
         {:noreply, assign(socket, game)}
 
       {:stop, game} ->
-        # TODO stop the timer
+        :timer.cancel(socket.assigns.timer_reference)
         {:noreply, assign(socket, game)}
     end
-  end
-
-  defp player_input_changeset(params) do
-    types = %{guessed_number: :integer}
-
-    {%PlayerInput{}, types}
-    |> Ecto.Changeset.cast(params, Map.keys(types))
   end
 end
